9e81e121234c9c0b8bb44f88370d04ed4b27314d
PierluigiCovone98 <covone.1895077@studenti.uniroma1.it>
2026-06-08 13:07:13 +0200

    Add the "train_neural_bigram" experiment to the code base

---
+++ experiments/train_neural_bigram.py
@@ -0,0 +1,10 @@
+"""Solve the "next character prediction problem" with a "neural" approach."""
+
+def main():
+    # The first thing we are going to do is: create the ``training set`` (inputs, xs, and targets, ys).
+    print("here we are")
+
+
+
+if __name__ == "__main__":
+    main()
